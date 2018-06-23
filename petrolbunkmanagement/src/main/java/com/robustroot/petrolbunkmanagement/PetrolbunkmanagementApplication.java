e93f9666864caedd9da7502521d10a787f382d83
chandrukumar <chandrukumar20893@gmail.com>
2018-06-23 18:01:04 +0530

    initial commit

---
+++ petrolbunkmanagement/src/main/java/com/robustroot/petrolbunkmanagement/PetrolbunkmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.robustroot.petrolbunkmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetrolbunkmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetrolbunkmanagementApplication.class, args);
+	}
+}
