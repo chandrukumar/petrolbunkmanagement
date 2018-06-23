331347011c0799e2451440dc18408081543b8f38
chandrukumar <chandrukumar20893@gmail.com>
2018-06-23 21:35:04 +0530

    column name is changed in both file for bunk entity

--- petrolbunkmanagement/src/main/java/com/robustroot/petrolbunkmanagement/entity/Bunk.java
+++ petrolbunkmanagement/src/main/java/com/robustroot/petrolbunkmanagement/entity/Bunk.java
@@ -1,16 +1,44 @@
 package com.robustroot.petrolbunkmanagement.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "bunk_details")
 public class Bunk {
 	@Id
+	@Column(name = "bunk_id")
 	Long bunkId;
 	String name;
 	String mobilenumber;
 	String password;
 	
+	public Long getBunkId() {
+		return bunkId;
+	}
+	public void setBunkId(Long bunkId) {
+		this.bunkId = bunkId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobilenumber() {
+		return mobilenumber;
+	}
+	public void setMobilenumber(String mobilenumber) {
+		this.mobilenumber = mobilenumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public Bunk(){
 		
 	}
